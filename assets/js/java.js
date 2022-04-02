c19474a454525e40d1efe8b3d2bd06471c344d3a
Alan Edlebeck <alan.edlebeck@gmail.com>
2022-04-02 18:30:45 -0500

    javascript complete

--- assets/js/java.js
+++ assets/js/java.js
@@ -26,13 +26,30 @@ function cityInfo (cityInput) {
     var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&appid=dac42aacf1187949ffc701f7f8725fad"
     fetch(geocodeURL)
     .then(function(response) {
+        if (response.ok) {
             return response.json()
+        } else {
+            alert("Something went wrong\nPlease try again")
+        }
     })
     .then(function(data) {
-        lat = data[0].lat
-        lon = data[0].lon
-        weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=dac42aacf1187949ffc701f7f8725fad&units=imperial"
-        weather()
+        if (data.length > 0){
+            lat = data[0].lat
+            lon = data[0].lon
+            weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=dac42aacf1187949ffc701f7f8725fad&units=imperial"
+            weather()
+        } else {
+            prevSearch.pop()
+            localStorage.setItem("prevSearch", JSON.stringify(prevSearch));
+            var parentDiv = document.getElementById("parent"+prevSearch.length)
+            var removeBtn = document.getElementById("prev"+prevSearch.length)
+            var searchDiv = document.getElementById("search")
+            parentDiv.removeChild(removeBtn)
+            searchDiv.removeChild(parentDiv)
+            placeholder.value = ''
+            placeholder.setAttribute("placeholder", "Enter City")
+            alert("Something went wrong\nCheck city spelling")
+        }
     })
 }
 
@@ -47,6 +64,8 @@ function weather () {
         currentTemp.innerText = "Temp:  " + data.current.temp + "\u00B0F"
         currentWind.innerText = "Wind:  " + data.current.wind_speed + " MPH"
         currentHumidity.innerText = "Humidity:  " + data.current.humidity + "%"
+        placeholder.value = ''
+        placeholder.setAttribute("placeholder", "Enter City")
         if (data.current.uvi <= 2) {
             currentUV.setAttribute("class","btn btn-success")
             currentUV.innerText = data.current.uvi
@@ -73,41 +92,45 @@ function weather () {
 }
 
 function getInput () {
+
     city = document.getElementById("cityInput").value;
     city = city.toUpperCase()
+    if (city != "") {
         if (prevSearch.indexOf(city) === -1) {
             prevSearch.push(city)
             var previous = document.createElement("div")
             previous.classList.add("d-flex", "justify-content-center")
+            previous.id = "parent" + (prevSearch.length-1)
             var prevBtn = document.createElement("button")
-            prevBtn.classList.add("btn", "btn-secondary", "m-1")
+            prevBtn.classList.add("btn", "btn-secondary", "m-1", "w-50")
             prevBtn.innerText = prevSearch[prevSearch.length-1]
             prevBtn.id = "prev" + (prevSearch.length-1)
+            prevBtn.addEventListener("click", function(){cityInfo(prevBtn.innerText)})
             previous.appendChild(prevBtn)
             var searchCol = document.getElementById("search")
             searchCol.appendChild(previous)
-            prevSearch.sort()
-        }
-        localStorage.setItem("prevSearch", JSON.stringify(prevSearch))
-        cityInfo(city)
+            localStorage.setItem("prevSearch", JSON.stringify(prevSearch));
+            cityInfo(city)
+        } 
     }
+}
 
 function previousSearch() {
     prevSearch = JSON.parse(localStorage.getItem("prevSearch"))
     if (prevSearch === null) {
         prevSearch = ["MILWAUKEE"]
     }
-    for (i=0; i < prevSearch.length; i++) {
+    prevSearch.forEach(function (result){
         var previous = document.createElement("div")
         previous.classList.add("d-flex", "justify-content-center")
         var prevBtn = document.createElement("button")
-        prevBtn.classList.add("btn", "btn-secondary", "m-1")
-        prevBtn.innerText = prevSearch[i]
-        prevBtn.id = "prev" + i
+        prevBtn.classList.add("btn", "btn-secondary", "m-1", "w-50")
+        prevBtn.innerText = result
+        prevBtn.addEventListener("click", function(){cityInfo(result)})
         previous.appendChild(prevBtn)
         var searchCol = document.getElementById("search")
         searchCol.appendChild(previous)
-     }
+    });
 }
 
 function removePlaceholder () {
